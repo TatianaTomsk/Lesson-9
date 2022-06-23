4c8f665a45555c4a09aa02d417c9b7764b8f1a30
TatianaTomsk <107985498+TatianaTomsk@users.noreply.github.com>
2022-06-23 08:15:31 +0700

    Add files via upload

---
+++ Project 9.py
@@ -0,0 +1,15 @@
+# Д/З № 2
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = {'Office': ['Яндекс (Москва)', 'Яндекс (Казань)', 'Яндекс (Екатеринбург)'],
+        'Stuff': [789, 543, 450],
+        'Number': [123, 345, 567]}
+        
+frame = pd.DataFrame(data)
+print(frame)
+
+frame1 = frame.query('Number < 350 & Stuff > 700')
+print(frame1)
